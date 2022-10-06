0bcf2f172adba385b095481c362602baaa9fe125
Piotr <pknocho1@stu.vistula.edu.pl>
2022-10-06 18:04:16 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldknochowski/HelloWorldKnochowski.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldknochowski;
+
+public class HelloWorldKnochowski {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Knochowski");
+    }
+}
